c27169367599e264f64c2768c0368206a7c791e7
Pedro Crespan <pedrocrespan@hotmail.com>
2022-09-28 07:46:18 -0300

    Added register function

--- app.py
+++ app.py
@@ -16,9 +16,9 @@ Session(app)
 def connection():
     conn = psycopg2.connect(
         host = 'localhost',
-        database = 'ONote',
-        user = 'ONote',
-        password = 'onote'
+        database = 'onote',
+        user = 'postgres',
+        password = 'postgres'
     )
     return conn
 
@@ -32,3 +32,31 @@ def require_login(f):
         else:
             return redirect("/login")
     return logged
+
+
+@app.route("/register", methods = ["GET", "POST"])
+def register():
+
+    con = connection()
+    cursor = con.cursor()
+
+    if request.method == "POST":
+        username = request.form.get("username")
+        password = request.form.get("password")
+        
+        user_exists = cursor.execute("SELECT id FROM users WHERE username = %s", (username, )).fetchall()
+        error = 'Username already in use'
+        if user_exists:
+            return render_template("/register", error = error)
+        else:
+            hashPassword = generate_password_hash(password, "sha256")
+            cursor.execute("INSERT INTO users (username, password) VALUES (%s, %s)", (username, hashPassword))
+    else:
+        return render_template("/register")
+
+
+@app.route("/login", methods = ["GET", "POST"])
+def login():
+    if request.method == "POST":
+        # Check if username and password match database
+        pass
